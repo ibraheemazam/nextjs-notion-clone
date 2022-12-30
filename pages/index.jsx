47e34e389f22e11d6b45bfc0a5b694117ab3fdf8
Ibraheem Azam <ibraheemazam@gmail.com>
2022-12-29 21:38:15 -0800

    commit before switching to data fetching branch

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,5 +7,5 @@ export default () => (
     <div sx={{variant: 'containers.page', display: 'flex', alignItems: 'center', height: '100%'}}>
       <h1 sx={{fontSize: 8, my: 0}}>This is a really dope note taking app.</h1>
     </div>
-  </div> 
+  </div>
 )
---
+++ src/data/data.js
@@ -0,0 +1,8 @@
+const notes = new Array(15)
+  .fill(1)
+  .map((_, i) => ({
+    id: Date.now() + 1,
+    title: `Note ${i}`
+  }))
+
+module.exports = notes;
